a6594dec70c31afa9acbb705f7238e76fdaf6042
Daniel Presas <daniel.presas@uoit.net>
2019-09-10 16:23:04 -0400

    [Update] main.cpp - BUGGED
    glfwWindow not being created...
    
    Also started a macro header for system messages in code

---
+++ Cappuccino/include/Cappuccino/CapMacros.h
@@ -0,0 +1,17 @@
+#ifndef CAP_MACROS_H
+#define CAP_MACROS_H
+
+#include <windows.h>
+#include <iostream>
+
+#if _DEBUG
+
+#define CAPP_PRINT(message, ...)			printf(message, ##__VA_ARGS__); printf("\n");
+#define CAPP_PRINT_ERROR(message, ...)	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),0x0C);\
+										printf(message, ##__VA_ARGS__);	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),0x07); printf("\n");
+
+#define SYS_EXIT(code)					system("pause"); exit(code);
+
+#endif
+
+#endif
---
+++ Cappuccino/src/Cappucino/main.cpp
@@ -0,0 +1,78 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <Cappuccino/CapMacros.h>
+
+#pragma region PROGRAM SETTINGS
+
+const GLuint  SCR_WIDTH		= 800;
+const GLuint  SCR_HEIGHT	= 600;
+const GLchar* SCR_NAME		= "Cappuccino Engine v1.0";
+
+#pragma endregion
+
+int main() {
+
+	#pragma region GLFW/GLAD SETUP
+
+	CAPP_PRINT("----------INITIALIZING GLFW----------");
+	//glfwInit();
+	if (!glfwInit()) {
+		CAPP_PRINT_ERROR("Error initializing GLFW!\nExiting...");
+		SYS_EXIT(-1);
+	}
+	CAPP_PRINT("Success! Setting GLFW Window Hints...")
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	
+	CAPP_PRINT("----------CREATING GLFW WINDOW----------");
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, SCR_NAME, NULL, NULL);
+
+	if(window == NULL) {
+		CAPP_PRINT_ERROR("Error creating GLFW window!\nExiting...");
+		glfwTerminate();
+		SYS_EXIT(-2);
+	}
+
+	CAPP_PRINT("Success! Setting window settings...");
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) { glViewport(0, 0, width, height); });
+
+
+	
+	CAPP_PRINT("----------INITIALIZING GLAD----------");
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		CAPP_PRINT_ERROR("Error initializing GLAD!\nExiting...");
+		glfwTerminate();
+		SYS_EXIT(-3);
+	}
+
+	CAPP_PRINT("Success! OpenGL function pointers loaded.");
+
+	#pragma endregion 
+
+	#pragma region RENDER LOOP
+
+	CAPP_PRINT("\n----------STARTING RENDER LOOP...\n")
+	while (!glfwWindowShouldClose(window)) {
+
+		// TODO: PROCESS INPUTS
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	#pragma	endregion
+
+	#pragma region PROGRAM TERMINATION
+
+	CAPP_PRINT("----------CLEANING UP AND EXITING...");
+
+	glfwTerminate();
+	SYS_EXIT(0);
+
+	#pragma	endregion
+}
